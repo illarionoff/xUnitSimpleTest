09b0684f131ec69c3f3434ae1c6b6a643fdce50d
UNIVERSE\aillarionov <illarionoff.a@gmail.com>
2023-03-23 21:15:36 +0100

    Initial commit

---
+++ TestApp.Test/UserManagementTest.cs
@@ -0,0 +1,58 @@
+﻿using TestApp.Functionality;
+using Xunit;
+
+namespace TestApp.Test;
+
+public class UserManagementTest
+{
+    [Fact]
+    public void AddUser_CreateUser()
+    {
+        // Arrange
+        UserManagement userManagement = new UserManagement();
+
+        // Act
+        userManagement.AddUser(new("John", "Doe"));
+
+        // Assert
+        var savedUser = Assert.Single(userManagement.AllUsers);
+        Assert.NotNull(savedUser);
+        Assert.Equal("John", savedUser.firstName);
+        Assert.Equal("Doe", savedUser.lastName);
+        Assert.False(savedUser.VerifiedEmail);
+    }
+
+    [Fact]
+    public void UpdatePhone_Update()
+    {
+        // Arrange
+        UserManagement userManagement = new UserManagement();
+
+        // Act
+        userManagement.AddUser(new("John", "Doe"));
+        var user = userManagement.AllUsers.First();
+        user.PhoneNumber = "123";
+        userManagement.UpdatePhone(user);
+        
+        // Assert
+        var savedUser = Assert.Single(userManagement.AllUsers);
+        Assert.NotNull(savedUser);
+        Assert.Equal("123", savedUser.PhoneNumber);
+    }
+
+    [Fact]
+    public void VerifyEmail_Verifye()
+    {
+        // Arrange
+        UserManagement userManagement = new UserManagement();
+
+        // Act
+        userManagement.AddUser(new("John", "Doe"));
+        userManagement.VerifyEmail(1);
+
+        // Assert
+        var savedUser = Assert.Single(userManagement.AllUsers);
+        Assert.NotNull(savedUser);
+        Assert.True(savedUser.VerifiedEmail);
+    }
+}
---
+++ TestApp/Functionality/UserManagement.cs
@@ -0,0 +1,33 @@
+﻿namespace TestApp.Functionality;
+
+public record User (string firstName, string lastName)
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public string PhoneNumber { get; set; }
+    public bool VerifiedEmail { get; set; } = false;
+}
+
+public class UserManagement
+{
+    private readonly List<User> _users = new();
+    private int idCounter = 1;
+
+    public IEnumerable<User> AllUsers => _users;
+    public void AddUser(User user)
+    {
+        _users.Add(user with {Id = idCounter++ });
+    }
+
+    public void UpdatePhone(User user)
+    {
+        var dbUSer = _users.First(x => x.Id == user.Id);
+        dbUSer.PhoneNumber= user.PhoneNumber;
+    }
+
+    public void VerifyEmail(int userId)
+    {
+        var dbUSer = _users.First(x => x.Id == userId);
+        dbUSer.VerifiedEmail = true;
+    }
+}
