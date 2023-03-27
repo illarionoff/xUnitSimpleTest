b3d9064dec71f2f02c3c0c59da87ceabb671c5c1
UNIVERSE\aillarionov <illarionoff.a@gmail.com>
2023-03-27 21:04:48 +0200

    Add manual injection

---
+++ TestApp.Test/ShoppingCartTest.cs
@@ -0,0 +1,104 @@
+﻿using TestApp.Functionality;
+using Xunit;
+
+namespace TestApp.Test;
+
+public class DbServiceMock : IDbService
+{
+    public bool Result { get; set; }
+    public Product  Product{ get; set; }
+    public int ProductId { get; set; }
+    bool IDbService.RemoveItemFromShoppingCart(int? productId)
+    {
+        if(productId == null)
+        {
+            return false; 
+        }
+
+        if (productId == 0)
+        {
+            return false;
+
+        }
+
+        ProductId = Convert.ToInt32(productId);
+        Result = true;
+        return Result;
+    }
+
+    bool IDbService.SaveItemToShoppingCart(Product? product)
+    {
+        if (product == null)
+        {
+            return false;
+        }
+
+        if (product.Id == 0)
+        {
+            return false;
+
+        }
+
+        Product = product;
+        return true;
+    }
+}
+
+public class ShoppingCartTest
+{
+    [Fact]
+    public void AddProduct_Success()
+    {
+        var dbMock = new DbServiceMock();
+        dbMock.Result = true;
+        var shoppingCart = new ShoppingCart(dbMock);
+
+        var product = new Product(1, "Shoes", 100);
+        var result = shoppingCart.AddProduct(product);
+
+        Assert.True(result);
+        Assert.Equal(result, dbMock.Result);
+        Assert.Equal(dbMock.Product.Id, product.Id);
+        Assert.Equal(dbMock.Product.Name, product.Name);
+        Assert.Equal(dbMock.Product.Price, product.Price);
+    }
+
+    [Fact]
+    public void AddProduct_Fail()
+    {
+        var dbMock = new DbServiceMock();
+        dbMock.Result = false;
+        var shoppingCart = new ShoppingCart(dbMock);
+
+        var result = shoppingCart.AddProduct(null);
+
+        Assert.False(result);
+        Assert.Equal(result, dbMock.Result);
+    }
+
+    [Fact]
+    public void RemoveProduct_Success()
+    {
+        var dbMock = new DbServiceMock();
+        dbMock.Result = true;
+        var shoppingCart = new ShoppingCart(dbMock);
+
+        var result = shoppingCart.DeleteProduct(1);
+
+        Assert.True(result);
+        Assert.Equal(result, dbMock.Result);
+    }
+
+    [Fact]
+    public void RemoveProduct_Fail()
+    {
+        var dbMock = new DbServiceMock();
+        dbMock.Result = false;
+        var shoppingCart = new ShoppingCart(dbMock);
+
+        var result = shoppingCart.DeleteProduct(0);
+
+        Assert.False(result);
+        Assert.Equal(result, dbMock.Result);
+    }
+}
---
+++ TestApp/Functionality/ShoppingCart.cs
@@ -0,0 +1,49 @@
+﻿namespace TestApp.Functionality;
+
+public record Product(int Id, string Name, double Price);
+
+public interface IDbService
+{
+    bool SaveItemToShoppingCart(Product? product);
+    bool RemoveItemFromShoppingCart(int? productId);
+}
+
+public class ShoppingCart
+{
+    private readonly IDbService _bService;
+
+    public ShoppingCart(IDbService bService)
+    {
+        _bService = bService;
+    }
+
+    public bool AddProduct(Product? product)
+    {
+        if (product == null)
+        {
+            return false;
+        }
+
+        if(product.Id == 0)
+        {
+            return false;
+        }
+
+        return _bService.SaveItemToShoppingCart(product);
+    }
+
+    public bool DeleteProduct(int? id)
+    {
+        if (id == null)
+        {
+            return false;
+        }
+
+        if (id == 0)
+        {
+            return false;
+        }
+
+        return _bService.RemoveItemFromShoppingCart(Convert.ToInt32(id));
+    }
+}
